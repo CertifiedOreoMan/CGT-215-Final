82ed5a26db186b50ce99e54e25db8d0af603b9fa
CertifiedBlyatman <tony.tunsakul@gmail.com>
2022-12-01 01:24:55 -0500

    Add project files.

---
+++ CGT 215 Final/CGT 215 Final.cpp
@@ -0,0 +1,170 @@
+// duckBuster.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+
+
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFPhysics.h>
+#include <vector>
+
+using namespace std;
+using namespace sf;
+using namespace sfp;
+
+const float KB_SPEED = 0.2;
+
+void LoadTex(Texture& tex, string filename) {
+    if (!tex.loadFromFile(filename)) {
+        cout << "Could not load " << filename << endl;
+    }
+}
+
+
+int main()
+{
+    RenderWindow window(VideoMode(800, 600), "Stickman Sprint");
+    World world(Vector2f(0, 0));
+    int score(0);
+
+    PhysicsSprite& stickman = *new PhysicsSprite();
+    Texture stickman1, stickman2;
+    LoadTex(stickman1, "images/stickman1.png");
+    LoadTex(stickman2, "images/stickman2.png");
+    stickman.setTexture(stickman1);
+    Vector2f sz = stickman.getSize();
+    stickman.setCenter(Vector2f(400,
+        600 - (sz.y / 2)));
+    bool running = true;
+    bool jumping = false;
+
+
+    /*PhysicsRectangle top;
+    top.setSize(Vector2f(800, 10));
+    top.setCenter(Vector2f(400, 5));
+    top.setStatic(true);
+    world.AddPhysicsBody(top);
+
+    PhysicsRectangle right;
+    right.setSize(Vector2f(10, 600));
+    right.setCenter(Vector2f(795, 300));
+    right.setStatic(true);
+    world.AddPhysicsBody(right);*/
+
+    Texture blockTex;
+    LoadTex(blockTex, "images/block.png");
+    PhysicsShapeList<PhysicsSprite> blocks;
+
+
+    /*top.onCollision = [&drawingArrow, &world, &arrow]
+    (PhysicsBodyCollisionResult result) {
+        drawingArrow = false;
+        world.RemovePhysicsBody(arrow);
+    };*/
+
+    Text scoreText;
+    Font font;
+    if (!font.loadFromFile("c:/windows/fonts/Arial.ttf")) {
+        cout << "Couldn't load font arial.ttf" << endl;
+        exit(1);
+    }
+    scoreText.setFont(font);
+    Text arrowCountText;
+    arrowCountText.setFont(font);
+
+    Clock clock;
+    Time lastTime(clock.getElapsedTime());
+    Time currentTime(lastTime);
+    long interval = 0;
+
+    while (running) {
+        currentTime = clock.getElapsedTime();
+        Time deltaTime = currentTime - lastTime;
+        int deltaMS = deltaTime.asMilliseconds();
+        interval += deltaMS;
+
+        if (deltaMS > 9) {
+            lastTime = currentTime;
+            world.UpdatePhysics(deltaMS);
+            //MoveCrossbow(crossBow, deltaMS);
+            if (Keyboard::isKeyPressed(Keyboard::Space) &&
+                !jumping) {
+                jumping = true;
+            }
+
+            window.clear();
+            for (PhysicsShape& block : blocks) {
+                window.draw((PhysicsSprite&)block);
+            }
+
+            if (interval >= 42000000) {
+                cout << "count" << endl;
+                bool anim = true;
+
+                if (anim == true) {
+                    stickman.setTexture(stickman2);
+                    anim = false;
+                }
+                else if (anim == false) {
+                    stickman.setTexture(stickman1);
+                    anim = true;
+                }
+
+                PhysicsSprite& block = blocks.Create();
+                block.setTexture(blockTex);
+                Vector2f sz = block.getSize();
+                block.setCenter(Vector2f(0, 20 + (sz.y / 2)));
+                block.setVelocity(Vector2f(0.25, 0));
+                world.AddPhysicsBody(block);
+
+                block.onCollision =
+                    [&running, &world, &block, &blocks, &score, &stickman]
+                (PhysicsBodyCollisionResult result) {
+                    if (result.object2 == stickman) {
+                        running = false;
+                    }
+
+                };
+                interval = 0;
+            }
+
+            window.draw(stickman);
+            scoreText.setString(to_string(score));
+            FloatRect textBounds = scoreText.getGlobalBounds();
+            scoreText.setPosition(
+                Vector2f(790 - textBounds.width, 590 - textBounds.height));
+            window.draw(scoreText);
+            //arrowCountText.setString(to_string(arrows));
+            textBounds = arrowCountText.getGlobalBounds();
+            arrowCountText.setPosition(
+                Vector2f(10, 590 - textBounds.height));
+            window.draw(arrowCountText);
+            //world.VisualizeAllBounds(window);
+
+            window.display();
+            //ducks.DoRemovals();
+        }
+    }
+    Text gameOverText;
+    gameOverText.setFont(font);
+    gameOverText.setString("GAME OVER");
+    FloatRect textBounds = gameOverText.getGlobalBounds();
+    gameOverText.setPosition(Vector2f(
+        400 - (textBounds.width / 2),
+        300 - (textBounds.height / 2)
+    ));
+    window.draw(gameOverText);
+    window.display();
+    while (true);
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
